1c7a48931b9ad3bab4eb787fd90f0dfb6fa7aeb9
RakibulB <rakibulbhuiyan1973@gmail.com>
2024-04-09 23:10:20 +0100

    added responsiveness to expense

--- src/components/Expense.jsx
+++ src/components/Expense.jsx
@@ -25,13 +25,19 @@ const Expense = ({ id, category, title, amount, date, onEdit }) => {
   return (
     <div
       onClick={() => onEdit({ id, category, title, amount, date })}
-      className="hover:cursor-pointer text-white w-full flex justify-between h-10 bg-purple-600 rounded-full align-middle items-center px-10 py-8 mb-5 overflow-hidden"
+      className="hover:cursor-pointer text-white w-full flex justify-between items-center bg-purple-600 rounded-full px-4 sm:px-6 lg:px-10 py-2 sm:py-4 lg:py-8 mb-2 sm:mb-3 lg:mb-5 overflow-hidden"
     >
       <div className="flex items-center">
-        <img className="w-10 h-10" src={icon} alt={category} />
-        <h1 className="ml-10 text-xl">{title}</h1>
+        <img
+          className="w-6 h-6 sm:w-8 sm:h-8 lg:w-10 lg:h-10"
+          src={icon}
+          alt={category}
+        />
+        <h1 className="ml-2 sm:ml-4 lg:ml-10 text-sm sm:text-base lg:text-xl truncate">
+          {title}
+        </h1>
       </div>
-      <p className="text-2xl">-£{amount}</p>
+      <p className="text-sm sm:text-lg lg:text-2xl truncate">-£{amount}</p>
     </div>
   );
 };
